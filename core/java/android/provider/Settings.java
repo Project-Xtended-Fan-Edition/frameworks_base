be59e3478bd6a569aa93cfcf2138c54ac6c63a20
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    QuickSettings: Add Haptic Feedback to tiles [1/2]
    [someone5678] Unify styles with other haptic settings
    
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6842,6 +6842,13 @@ public final class Settings {
         public static final String QS_SHOW_DATA_USAGE = "qs_show_data_usage";
 
         /**
+         * Give haptic feedback when quick settings tiles are pressed.
+         * @hide
+         */
+        @Readable
+        public static final String QS_TILES_HAPTIC_FEEDBACK = "qs_tiles_haptic_feedback";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
