96f58ec133d9787cf0afbd954937ea653dfbb3af
Pranav Vashi <neobuddy89@gmail.com>
2024-10-06 01:24:30 +0530

    SystemUI: Allow to toggle BT directly with Bluetooth tile [1/2]
    * We do not want to lose default behavior that matches with the internet tile
      The default behavior is to show dialog.
    * However, disabling dialog - will match the behavior with the wifi tile.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6918,6 +6918,12 @@ public final class Settings {
         public static final String ENABLE_RIPPLE_EFFECT = "enable_ripple_effect";
 
         /**
+         * Whether to show Bluetooth dialog or toggle bluetooth using Bluetooth tile
+         * @hide
+         */
+        public static final String QS_BT_SHOW_DIALOG = "qs_bt_show_dialog";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
