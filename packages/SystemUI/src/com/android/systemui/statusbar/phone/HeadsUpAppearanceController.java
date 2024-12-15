7f64080b073b69c3a2ecab4e9f3e4c34deb7800d
LuK1337 <priv.luk@gmail.com>
2024-12-16 00:01:58 +0200

    fixup! SystemUI: Clock position customization
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I18f792e21c4c3f4d537b7723752237caf52b92b2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -45,6 +45,7 @@ import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow
 import com.android.systemui.statusbar.notification.row.shared.AsyncGroupHeaderViewInflation;
 import com.android.systemui.statusbar.notification.stack.NotificationRoundnessManager;
 import com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayoutController;
+import com.android.systemui.statusbar.phone.PhoneStatusBarViewController;
 import com.android.systemui.statusbar.phone.fragment.dagger.StatusBarFragmentScope;
 import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
@@ -132,7 +133,8 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
             FeatureFlagsClassic featureFlags,
             HeadsUpNotificationIconInteractor headsUpNotificationIconInteractor,
             @Named(OPERATOR_NAME_FRAME_VIEW) Optional<View> operatorNameViewOptional,
-            @RootView PhoneStatusBarView statusBarView) {
+            @RootView PhoneStatusBarView statusBarView,
+            PhoneStatusBarViewController statusBarViewController) {
         super(headsUpStatusBarView);
         mNotificationRoundnessManager = notificationRoundnessManager;
         mHeadsUpManager = headsUpManager;
@@ -154,7 +156,7 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
         mClockView = clockView;
         mOperatorNameViewOptional = operatorNameViewOptional;
         mDarkIconDispatcher = darkIconDispatcher;
-        mClockController = statusBarView.getClockController();
+        mClockController = statusBarViewController.getClockController();
 
         mView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             @Override
